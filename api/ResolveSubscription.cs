9f25cede02f38538474cf394f5e61b69100c48fe
ercenk <ercenk@ercenk.com>
2020-12-22 15:02:24 -0800

    Modify subtree

--- api/ResolveSubscription.cs
+++ api/ResolveSubscription.cs
@@ -1,17 +1,33 @@
-using System;
-using System.IO;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace Microsoft.CommercialMarketplace
+{
+    using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+    using System;
 
-namespace Microsoft.CommercialMarketplace
-{
     public static class ResolveSubscription
     {
+            private static IConfiguration Configuration { set; get; }
+static ResolveSubscription()
+{
+
+     var builder = new ConfigurationBuilder();
+    builder.AddAzureAppConfiguration()
+                .AddUserSecrets();
+    Configuration = builder.Build();
+    
+}
+
         [FunctionName("ResolveSubscription")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
@@ -20,6 +36,8 @@ namespace Microsoft.CommercialMarketplace
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["token"];
+
+            
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
